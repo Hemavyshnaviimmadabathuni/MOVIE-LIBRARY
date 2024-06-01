ab4cc9427c6da21bfde2e658aaab2fae97b5d7b8
Hemavyshnaviimmadabathuni <109737015+Hemavyshnaviimmadabathuni@users.noreply.github.com>
2024-06-01 13:15:21 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,35 @@
 const express = require('express');
+const app = express();
 const router = express.Router();
-const {
-  ensureAuthenticated,
-  forwardAuthenticated
-} = require('../config/auth');
+const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
 const mongoose = require('mongoose');
+const Playlist = require('../models/Playlist');
 
-const Playlist = require('../models/Playlist')
+// Middleware (e.g., body-parser, if needed)
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
+// Set view engine to ejs
+app.set('view engine', 'ejs');
+
+// Define routes
 // Welcome Page
 router.get('/', forwardAuthenticated, (req, res) => res.render('welcome'));
 
 // Dashboard
-router.get('/home', ensureAuthenticated, (req, res) =>
-  res.render('home', {
-    user: req.user
-  })
+router.get('/home', ensureAuthenticated, (req, res) => 
+  res.render('home', { user: req.user })
 );
 
-//movie app pages
+// Movie app pages
 router.get('/watchlists', ensureAuthenticated, (req, res) => {
   const useremail = req.user.email;
-  const query = {
-    email: useremail
-  };
+  const query = { email: useremail };
 
   Playlist.distinct("playlistname", query).then((ans) => {
-    res.render('watchlists', {
-      user: req.user,
-      playlistarray: ans
-    });
-  })
-})
+    res.render('watchlists', { user: req.user, playlistarray: ans });
+  });
+});
 
 router.post('/watchlists', ensureAuthenticated, (req, res) => {
   const movieid = req.body.movieid;
@@ -40,40 +38,36 @@ router.post('/watchlists', ensureAuthenticated, (req, res) => {
   if (!newname) {
     newname = 'myplaylist';
   }
-  // console.log(movieid + " + " + newname + " + " + useremail);
+
   const newPlaylist = new Playlist({
     email: useremail,
     movieId: movieid,
     playlistname: newname
   });
-  newPlaylist.save()
+  newPlaylist.save();
 
-  const query = {
-    email: useremail
-  };
+  const query = { email: useremail };
   Playlist.distinct("playlistname", query).then((ans) => {
-    res.render('watchlists', {
-      user: req.user,
-      playlistarray: ans
-    });
-  })
-})
+    res.render('watchlists', { user: req.user, playlistarray: ans });
+  });
+});
 
 router.post('/watchlists/viewplaylist', ensureAuthenticated, (req, res) => {
   const useremail = req.user.email;
   const playlistname = req.body.playlistname;
-  const query = {
-    playlistname: playlistname,
-    email: useremail
-  };
+  const query = { playlistname: playlistname, email: useremail };
+
   Playlist.distinct("movieId", query).then((ans) => {
-      res.render('viewplaylist', {
-        user: req.user,
-        playlistname: playlistname,
-        movies: ans
-      });
-  })
+    res.render('viewplaylist', {
+      user: req.user,
+      playlistname: playlistname,
+      movies: ans
+    });
+  });
+});
 
-})
+// Use the router
+app.use('/', router);
 
-module.exports = router;
+// Export the Express app
+module.exports = app;
